261790ccdb073b3e79c1e10b603a86ba5e2501d2
Thais Gonçalves <thais.gosil@gmail.com>
2020-12-02 17:50:10 -0300

    navbar, footer, sobre nos

--- Frontend/agreeks-ui/src/app/app-routing.module.ts
+++ Frontend/agreeks-ui/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'sobre-nos', component: Sob }
+  { path: 'sobre-nos', component: SobreNosComponent }
 ];
 
 @NgModule({
